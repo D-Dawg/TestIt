7bfd6f4fcf2275069cadeec216c9bfdec97f5cbe
David Oberhofer <oberhofer.david@gmail.com>
2016-08-06 19:02:41 +0200

    added test route

---
+++ app/routes/test.js
@@ -0,0 +1,54 @@
+//MIT License
+
+//Copyright (c) 2016 David Oberhofer
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the 'Software'), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+(function () {
+    'use strict';
+    const Promise = require('bluebird');
+    const User = require('../models/User');
+    const Template = require('../models/Template');
+    const Test = require('../models/Test');
+    const AccessToken = require('../models/AccessToken');
+    const crypto = require('../crypto');
+    const logger = require('proxey-ilogger')('Route:Test');
+    const template = require('../enum/template');
+    const mailer = require('../mailer');
+    const permission = require('../enum/permission');
+    const requiresPermission = require('./middleware/requires-permission');
+    let router = require('express').Router();
+
+
+    router.post('/assign', requiresPermission(permission.VIEW_TEMPLATE), Promise.coroutine(function* (req, res) {
+        if(typeof req.body === "object" && typeof req.body._id === "string" && typeof req.body.assignee === "string") {
+            let test = yield Test.findById(req.body._id);
+            if(test !== null) {
+                test.assignee = req.body.assignee;
+                res.send(yield test.save());
+            } else {
+                res.sendStatus(500);
+            }
+        } else {
+            res.sendStatus(401);
+        }
+    }));
+
+
+    module.exports = router;
+})();
