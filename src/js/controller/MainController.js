dc8bc4e8f427b86aac124e898401083248fd6103
David Oberhofer <oberhofer.david@gmail.com>
2016-08-15 09:40:10 +0200

    load enums

--- src/js/controller/MainController.js
+++ src/js/controller/MainController.js
@@ -1,11 +1,15 @@
 window.testit.controller('MainController', ['$scope', '$http', function ($scope, $http) {
     'use strict';
 
+    $http.get('/enums').then(response => {
+        $scope.enums = response.data;
+    });
+
     $http.get('/personal/session').then(response => {
         $scope.me = response.data.me;
-
+        $scope.users = response.data.users;
         $scope.permissions = {};
-        for(let i = 0; i < response.data.permissions.length; i++) {
+        for (let i = 0; i < response.data.permissions.length; i++) {
             $scope.permissions[response.data.permissions[i]] = response.data.me.permissions.indexOf(response.data.permissions[i]) > -1;
         }
     });
